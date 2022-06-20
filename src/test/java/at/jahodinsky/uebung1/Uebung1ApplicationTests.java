f97e4ffca2f2eef9929be57a26c99dcf375bf7fb
wi21b009 <wi21b009@technikum-wien.at>
2022-06-20 10:45:01 +0200

    Initial commit

---
+++ src/test/java/at/jahodinsky/uebung1/Uebung1ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.jahodinsky.uebung1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uebung1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
